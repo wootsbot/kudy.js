f27053c0fe38c8c28f0871e5d458da4933022acd
Jorge Luis Calleja <jorgelca1205@gmail.com>
2019-11-06 01:38:32 -0600

    [core]: Add config for soport language

--- src/config-default.js
+++ src/config-default.js
@@ -2,10 +2,12 @@ const phrases = require("./phrases");
 
 const DEFAULT_NUMBER_RANDOM = null;
 const DEFAULT_LIST_PHRASES = phrases;
+const LNG = "es";
 
 const configDefault = {
   defaultNumberRandom: DEFAULT_NUMBER_RANDOM,
-  phrases: DEFAULT_LIST_PHRASES
+  phrases: DEFAULT_LIST_PHRASES,
+  lng: LNG
 };
 
 module.exports = configDefault;
